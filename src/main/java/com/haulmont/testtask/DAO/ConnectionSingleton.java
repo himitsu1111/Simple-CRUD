da9e72c7e43f46058ae3d62aa41a0772c583c1c9
himitsu1111 <lettercatcher@gmail.com>
2017-04-10 14:51:28 +0400

    to deploy 7

--- src/main/java/com/haulmont/testtask/DAO/ConnectionSingleton.java
+++ src/main/java/com/haulmont/testtask/DAO/ConnectionSingleton.java
@@ -15,11 +15,11 @@ public class ConnectionSingleton {
             try {
                 Class.forName("org.postgresql.Driver");
 
-                instance = DriverManager.getConnection("DATABASE_URL");
+//                instance = DriverManager.getConnection("DATABASE_URL");
 
-//                instance = DriverManager.getConnection("jdbc:postgresql://ec2-54-228-235-185.eu-west-1.compute.amazonaws.com" +
-//                        ":5432/decn6648c290uc?user=sfhyuufaikwdbp&password=b777edc6ead336752968de64c492cda7fb0a1a1aac847487c6793024560595e3&" +
-//                        "ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+                instance = DriverManager.getConnection("jdbc:postgresql://ec2-54-228-235-185.eu-west-1.compute.amazonaws.com" +
+                        ":5432/decn6648c290uc?user=sfhyuufaikwdbp&password=b777edc6ead336752968de64c492cda7fb0a1a1aac847487c6793024560595e3&" +
+                        "ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
 //                "jdbc:postgresql://ec2-107-20-214-225.compute-1.amazonaws.com:5432/databasename?user=someusername&" +
 //                        "password=somepassword&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
             } catch (SQLException e) {
