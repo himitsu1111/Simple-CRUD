7effaa3967f53a91f2c14cc8d16f36c43fb3b183
himitsu1111 <lettercatcher@gmail.com>
2017-04-11 07:57:13 +0400

    work

--- src/main/java/com/haulmont/testtask/DAO/ConnectionSingleton.java
+++ src/main/java/com/haulmont/testtask/DAO/ConnectionSingleton.java
@@ -13,15 +13,8 @@ public class ConnectionSingleton {
     public static synchronized Connection getInstance() {
         if (instance == null) {
             try {
-                Class.forName("org.postgresql.Driver");
-
-//                instance = DriverManager.getConnection("DATABASE_URL");
-
-                instance = DriverManager.getConnection("jdbc:postgresql://ec2-54-228-235-185.eu-west-1.compute.amazonaws.com" +
-                        ":5432/decn6648c290uc?user=sfhyuufaikwdbp&password=b777edc6ead336752968de64c492cda7fb0a1a1aac847487c6793024560595e3&" +
-                        "ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
-//                "jdbc:postgresql://ec2-107-20-214-225.compute-1.amazonaws.com:5432/databasename?user=someusername&" +
-//                        "password=somepassword&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+                Class.forName("org.hsqldb.jdbcDriver");
+                instance = DriverManager.getConnection("jdbc:hsqldb:file:C:\\db\\hsqldb\\demodb", "sa", "");
             } catch (SQLException e) {
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
